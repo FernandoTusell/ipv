71d4eae539877491851b6678d329a7115e8ce4ef
F.Tusell <fernando.tusell@ehu.es>
2020-01-09 14:00:53 +0100

    Added vignette in progress.

---
+++ vignettes/ejemplo-uso.R
@@ -0,0 +1,4 @@
+## ----setup, include=FALSE---------------------------------------------------------------------------------------------
+knitr::opts_chunk$set(cache=TRUE, fig.width=7, fig.height=5.5)
+options(width=120)
+
